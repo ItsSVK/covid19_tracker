4892080de77ddcfe54b075608122e8b383c1d107
ItsSVK <35792535+ItsSVK@users.noreply.github.com>
2021-01-30 11:23:42 +0530

    Updates

---
+++ precache-manifest.f9bf447e88220d2d61469e4d95d67604.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02bd1d149dc3894193ee7e0bbefb2e0",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "76c0ff3a296d7129a754",
+    "url": "/covid19_tracker/static/css/main.30a934fc.chunk.css"
+  },
+  {
+    "revision": "8a5ed13267e571780616",
+    "url": "/covid19_tracker/static/js/2.e88c4a3a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_tracker/static/js/2.e88c4a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c0ff3a296d7129a754",
+    "url": "/covid19_tracker/static/js/main.f773334c.chunk.js"
+  },
+  {
+    "revision": "c49f83adef00a65af772",
+    "url": "/covid19_tracker/static/js/runtime-main.21c701a8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19_tracker/static/media/image.d7265326.png"
+  }
+]);
